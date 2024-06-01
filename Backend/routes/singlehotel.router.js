8045ce9bfd7a73f1704d44876d1d2d5f11cf811d
Love4sharma <www.lovesharma2004@gmail.com>
2024-06-01 19:16:52 +0530

    travel app

---
+++ Backend/routes/singlehotel.router.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const singlehotelHandler = require("../controllers/singleHoterController");
+
+router.route("/:id").get(singlehotelHandler);
+
+module.exports = router;
